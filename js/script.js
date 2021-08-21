24e131a4d1c1f5a580b5cb4a1205f63b947ecbc0
Noteventryin <tony1730280546@gmail.com>
2021-08-21 11:06:59 -0400

    Final update going for Exceeds
    Added searchbar with data filter for first and last name search

--- js/script.js
+++ js/script.js
@@ -18,30 +18,37 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 //use getElementsByClassName method to select the class('student-list')[0]
+const linklist = document.getElementsByClassName('link-list')[0];
+const studentlist = document.getElementsByClassName('student-list')[0];
+let itemData = data;
 function showPage(list ,page){
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
-   const studentlist = document.getElementsByClassName('student-list')[0];
    studentlist.innerHTML = '';
-for (let i = 0; i < list.length; i++){
+   let studentInfo = '';
+      //If no results found during search
+      if(list.length === 0){
+         studentInfo += `<p class="no-results">No Results Found</p>`;
+      } else {
+       for (let i = 0; i < list.length; i++){
       if (i >= startIndex && i < endIndex){
-         studentlist.insertAdjacentHTML(
-         'beforeend', 
-      `<li class="student-item cf">
-         <div class="student-details">
-            <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
-            <h3>${data[i].name.first} ${data[i].name.last}</h3>
-            <span class="email">${data[i].email}</span>
-         </div>
-         <div class="joined-details">
-            <span class="date">Joined ${data[i].registered.date}</span>
-         </div>   
-      </li>`);
+         studentInfo += `
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>    
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+          </div>
+         </li>`;
+
+     }
+    }
   }
- }
+  studentlist.insertAdjacentHTML("beforeend", studentInfo);
 }
-showPage(data, 1);
-//Called the function use the data variable and page number: 1  as arguments
 //used console.log() to test code and check all variables
 
 /*
@@ -50,7 +57,6 @@ This function will create and insert/append the elements needed for the paginati
 */
 function createbutton(list){
    let pagenumber = Math.ceil(list.length/9);
-   let linklist = document.getElementsByClassName('link-list')[0];
    linklist.innerHTML = '';
 // Math.ceil always rounds a number up to the next largest integer. 
 for(let i = 1; i <= pagenumber; i++){
@@ -58,11 +64,37 @@ linklist.insertAdjacentHTML('beforeend',
    `<li>
       <button type="button">${i}</button>
    </li>`);
-} 
+ }
+
    const firstbutton = document.querySelector('button');
-   firstbutton.setAttribute("class","active");   
+   firstbutton.setAttribute("class","active");
+}   
  //setAttribute() method adds the specified attribute to an element, and gives it the specified value.
- linklist.addEventListener('click',(e) =>{
+ function insertSearchBar() {
+   const header = document.querySelector('.header');
+   searchBarHTML = `
+   <label for="search" class="student-search">
+            <span>Search by name</span>
+            <input id="search" placeholder="Search by name...">
+            <button type="button" class="submit"><img src="img/icn-search.svg" alt="Search icon"></button>
+          </label>`;
+   header.insertAdjacentHTML("beforeend", searchBarHTML);
+}
+
+
+
+// Call functions
+showPage(data,1);
+createbutton(data);
+insertSearchBar();
+
+// Variables for search bar
+const searchField = document.getElementById('search');
+const searchBtn = document.querySelector('button.submit');
+
+
+//click event for pagination buttons
+linklist.addEventListener('click',(e) =>{
    if(e.target.tagName === 'BUTTON'){
       const removebutton = document.querySelector('.active');
       removebutton.className = '';
@@ -71,15 +103,26 @@ linklist.insertAdjacentHTML('beforeend',
       const display = addbutton.textContent;
       showPage(list,display);
    } 
-   //removed the active class first then add it to the event target
-   //call the showPage function passing 2 arguments 
-
  });
-}
+//removed the active class first then add it to the event target
+//call the showPage function passing 2 arguments 
+// Click event for search bar
+searchField.addEventListener('keyup', () => {
+   let searchText = searchField.value.toUpperCase();
+   searchBtn.onclick = () => {
+      searchField.value = '';
+   }
 
-
-// Call functions
-showPage(data,1);
-createbutton(data);
-
-
+   const filteredList = data.filter(student => {
+      return (
+         student.name.first.toUpperCase().includes(searchText) ||
+         student.name.last.toUpperCase().includes(searchText)
+      );
+   });
+   itemData = filteredList;
+   currentPage = 1
+   showPage(itemData, currentPage);
+   createbutton(itemData);
+ 
+});
+//return student.name.first or .last
